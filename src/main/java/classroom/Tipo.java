d2d322e243d5d0e7b4895b4b0d6fc077e169e971
Yiduaaar <124937930+Yiduaaar@users.noreply.github.com>
2023-09-05 09:41:00 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,12 +1,34 @@
 package classroom;
 
 public enum Tipo {
-	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
+	 DISCIPLINAR(10), FUNDAMENTACION(20), ELECTIVA(30);
 	
-	private int codigo;
-	private String nombre;
-	public Tipo(int codigo,String nombre) {
-		this.codigo = codigo;
-		this.nombre = nombre;
-	}
+		private int codigo;
+		private String nombre;
+		
+		private Tipo(int valor){
+			this.codigo = valor;
+		}
+
+		private Tipo(int codigo,String nombre) {
+			this.codigo = codigo;
+			this.nombre = nombre;
+		}
+		
+		public int getCodigo() {
+			return codigo;
+		}
+		
+		public void setCodigo(int codigo) {
+			this.codigo = codigo;
+		}
+		
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+
 }
